c36c4796dfb69677d6c198029fdd9b1110e4356c
charxie <charxie@gmail.com>
2014-04-11 08:34:02 -0400

    All screens done

---
+++ web/screen2.dart
@@ -0,0 +1,17 @@
+import 'dart:html';
+
+final HtmlElement runButton = querySelector("#run_button");
+final VideoElement video = querySelector("#video-2");
+final HtmlElement appletWindow = querySelector("#applet_window");
+
+void main() {
+
+  video.onEnded.listen((e) => _showApplet());
+  
+}
+
+void _showApplet() {
+  video.style.visibility = "hidden";
+  appletWindow.style.visibility = "visible";
+  runButton.click();
+}
---
+++ web/screen4.dart
@@ -0,0 +1,17 @@
+import 'dart:html';
+
+final HtmlElement runButton = querySelector("#run_button");
+final VideoElement video = querySelector("#video-4");
+final HtmlElement appletWindow = querySelector("#applet_window");
+
+void main() {
+
+  video.onEnded.listen((e) => _showApplet());
+  
+}
+
+void _showApplet() {
+  video.style.visibility = "hidden";
+  appletWindow.style.visibility = "visible";
+  runButton.click();
+}
